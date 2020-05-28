279ea7ab36be5f5e56df8da507416cf31fe8930d
Samer <samerkkr@gmail.com>
2020-05-28 02:57:53 -0300

    implemented searchBar

--- client/src/components/PasswordList.jsx
+++ client/src/components/PasswordList.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import SinglePasswordCard from './SinglePasswordCard';
 
 function PasswordList({ passwordList }) {
 	const renderList = () => {
+		if (searchInput.length !== 0) {
+			return filteredList.map((el) => {
+				return (
+					<div className="col-md-6" key={`${el._id}`}>
+						<SinglePasswordCard
+							_id={el._id}
+							domain={el.domain}
+							userName={el.userName}
+							password={el.password}
+							form={`${el._id}`}
+							initialValues={{
+								domain: el.domain,
+								userName: el.userName,
+								password: el.password,
+							}}
+						/>
+					</div>
+				);
+			});
+		}
 		return passwordList.map((el) => {
 			return (
 				<div className="col-md-6" key={`${el._id}`}>
@@ -24,9 +44,32 @@ function PasswordList({ passwordList }) {
 			);
 		});
 	};
-
+	const [searchInput, setSearchInput] = useState('');
+	const [filteredList, setFilteredList] = useState([]);
+	const findElements = (e) => {
+		const x = passwordList.filter((el) => {
+			if (
+				el.domain.startsWith(e.target.value) ||
+				el.userName.startsWith(e.target.value) ||
+				el.password.startsWith(e.target.value)
+			) {
+				return el;
+			}
+		});
+		setFilteredList(x);
+	};
 	return (
 		<div className="container">
+			<div className="row">
+				<input
+					style={{ margin:'auto auto',marginTop:"10px"}}
+					value={searchInput}
+					onChange={(e) => {
+						setSearchInput(e.target.value);
+						findElements(e);
+					}}
+				/>
+			</div>
 			<div className="row">{renderList()}</div>
 		</div>
 	);
