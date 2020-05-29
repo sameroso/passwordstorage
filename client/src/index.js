088619bf10e75fafe8a69e452bb27cf82ed35864
Samer <samerkkr@gmail.com>
2020-05-28 21:35:00 -0300

    favicon test

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import { toast } from 'react-toastify';
+import Favicon from 'react-favicon';
 
 import reducers from './reducers';
 import App from './components/App';
 
-import axios from 'axios';
-window.axios = axios;
 
 toast.configure();
 
@@ -17,6 +16,7 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
+		<Favicon url="./favicon.ico"/>
 		<App />
 	</Provider>,
 	document.getElementById('root')
