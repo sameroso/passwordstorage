6d62373870f80032d19e9d8c67a9359a06947c68
sameroso <samerkkr@gmail.com>
2020-06-06 23:35:40 -0300

    prettier

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,52 +2,51 @@ const passport = require('passport');
 const decrypt = require('../services/decrypt');
 
 const app = (app) => {
-	app.get(
-		'/auth/google',
-		passport.authenticate('google', { scope: ['profile'] })
-	);
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+  );
 
-	app.get(
-		'/auth/google/callback',
-		passport.authenticate('google', {
-			failureRedirect: '/login',
-			failureFlash: true,
-		}),
-		function (req, res) {
-			// Successful authentication, redirect home.
-			res.redirect('/');
-		}
-	);
-	app.get('/auth/facebook', passport.authenticate('facebook'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+      failureRedirect: '/login',
+      failureFlash: true,
+    }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
+  );
+  app.get('/auth/facebook', passport.authenticate('facebook'));
 
-	app.get(
-		'/auth/facebook/callback',
-		passport.authenticate('facebook', {
-			failureRedirect: '/login',
-			failureFlash: true,
-		}),
-		function (req, res) {
-			// Successful authentication, redirect home.
-			res.redirect('/');
-		}
-	);
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      failureRedirect: '/login',
+      failureFlash: true,
+    }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
+  );
 
-	app.get('/api/current', (req, res) => {
-		if(!req.user){
-			res.send(req.user);
-		}else{
-			const decrypting = req.user.passwordList.map((data) => {
-				data.password = decrypt(data.password)
-			})
-			res.send(req.user);
-		}
-	});
+  app.get('/api/current', (req, res) => {
+    if (!req.user) {
+      res.send(req.user);
+    } else {
+      const decrypting = req.user.passwordList.map((data) => {
+        data.password = decrypt(data.password);
+      });
+      res.send(req.user);
+    }
+  });
 
-	app.get('/api/logout', (req, res) => {
-		req.logout();
-		res.redirect('/');
-	  });
-	  
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
 };
 
 module.exports = app;
--- services/passport.js
+++ services/passport.js
@@ -7,94 +7,94 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id).then((user) => {
-		done(null, user);
-	});
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
-			proxy: true,
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			try {
-				const existingUser = await User.findOne({ googleId: profile.id });
-				if (existingUser) {
-					if (
-						existingUser.profileName === profile.name.givenName &&
-						existingUser.profilePic === profile._json.picture
-					) {
-						return done(null, existingUser);
-					} else if (existingUser.profileName !== profile.name.givenName) {
-						existingUser.profileName = profile.name.givenName;
-						const userUpdated = await existingUser.save();
-						return done(null, userUpdated);
-					} else if (existingUser.profilePic !== profile._json.picture) {
-						console.log('pic changed');
-						existingUser.profilePic = profile._json.picture;
-						const userUpdated = await existingUser.save();
-						return done(null, userUpdated);
-					} else {
-						existingUser.profileName = profile.name.givenName;
-						existingUser.profilePic = profile._json.picture;
-						const userUpdated = await existingUser.save();
-						return done(null, userUpdated);
-					}
-				}
-				const user = await new User({
-					googleId: profile.id,
-					profilePic: profile._json.picture,
-					profileName: profile.name.givenName,
-				}).save();
-				done(null, user);
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	)
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+          if (
+            existingUser.profileName === profile.name.givenName &&
+            existingUser.profilePic === profile._json.picture
+          ) {
+            return done(null, existingUser);
+          } else if (existingUser.profileName !== profile.name.givenName) {
+            existingUser.profileName = profile.name.givenName;
+            const userUpdated = await existingUser.save();
+            return done(null, userUpdated);
+          } else if (existingUser.profilePic !== profile._json.picture) {
+            console.log('pic changed');
+            existingUser.profilePic = profile._json.picture;
+            const userUpdated = await existingUser.save();
+            return done(null, userUpdated);
+          } else {
+            existingUser.profileName = profile.name.givenName;
+            existingUser.profilePic = profile._json.picture;
+            const userUpdated = await existingUser.save();
+            return done(null, userUpdated);
+          }
+        }
+        const user = await new User({
+          googleId: profile.id,
+          profilePic: profile._json.picture,
+          profileName: profile.name.givenName,
+        }).save();
+        done(null, user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  )
 );
 
 passport.use(
-	new FacebookStrategy(
-		{
-			clientID: keys.facebookAppID,
-			clientSecret: keys.facebookClientSecret,
-			callbackURL: '/auth/facebook/callback',
-			profileFields: ['name', 'id', 'picture.type(large)', 'displayName'],
-			proxy: true,
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			const profilePhoto = profile.photos
-				? profile.photos[0].value
-				: '/img/faces/unknown-user-pic.jpg';
-			try {
-				const existingUser = await User.findOne({ facebookId: profile.id });
-				if (existingUser) {
-					existingUser.profilePic = profilePhoto;
-					existingUser.profileName = profile.name.givenName;
-					const userUpdated = await existingUser.save();
-					return done(null, userUpdated);
-				}
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['name', 'id', 'picture.type(large)', 'displayName'],
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const profilePhoto = profile.photos
+        ? profile.photos[0].value
+        : '/img/faces/unknown-user-pic.jpg';
+      try {
+        const existingUser = await User.findOne({ facebookId: profile.id });
+        if (existingUser) {
+          existingUser.profilePic = profilePhoto;
+          existingUser.profileName = profile.name.givenName;
+          const userUpdated = await existingUser.save();
+          return done(null, userUpdated);
+        }
 
-				const user = await new User({
-					facebookId: profile.id,
-					profilePic: profile.photos
-						? profile.photos[0].value
-						: '/img/faces/unknown-user-pic.jpg',
-					profileName: profile.name.givenName,
-				}).save();
-				done(null, user);
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	)
+        const user = await new User({
+          facebookId: profile.id,
+          profilePic: profile.photos
+            ? profile.photos[0].value
+            : '/img/faces/unknown-user-pic.jpg',
+          profileName: profile.name.givenName,
+        }).save();
+        done(null, user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  )
 );
