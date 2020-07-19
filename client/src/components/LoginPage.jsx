f21fed29ac86fa39ba364da652b23e4cc953d13b
sameroso <samerkkr@gmail.com>
2020-07-19 17:40:09 -0300

    about us login page

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -2,38 +2,43 @@ import React from 'react';
 import LoginBtns from './LoginBtns';
 import './LoginPage.css';
 import passwordLogo from '../passwordlogo.png';
-
+import { Link } from 'react-router-dom';
 
 function LoginPage() {
-	return (
-		<div className="size d-flex bg-login">
-			<div className="container d-flex">
-				<div className="my-auto mx-auto">
-				
-					<div className="row">
-						<img
-							className="mx-auto img-fluid logo-size"
-							src={passwordLogo}
-							alt="logo"
-						/>
-					</div>
-					<div className="row tittle">
-						<h2 className="mx-auto mb-4 font">
-							<strong>Password</strong> Storage
-						</h2>
-					</div>
-					<div className="row">
-						<p className="mx-auto font">
-							Store your passwords safely	
-						</p>
-					</div>
-					<div className="mt-5">
-						<LoginBtns />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="size d-flex bg-login">
+      <div className="container d-flex">
+        <div className="my-auto mx-auto">
+          <div className="row">
+            <img
+              className="mx-auto img-fluid logo-size"
+              src={passwordLogo}
+              alt="logo"
+            />
+          </div>
+          <div className="row tittle">
+            <h2 className="mx-auto mb-4 font">
+              <strong>Password</strong> Storage
+            </h2>
+          </div>
+          <div className="row">
+            <p className="mx-auto font">Store your passwords safely</p>
+          </div>
+          <div className="row mt-5">
+            <Link to="/aboutus" className="mx-auto">
+              <p className="mx-auto font text-center mx-auto font-about-us">
+                About Us
+              </p>
+            </Link>
+          </div>
+
+          <div className="mt-5">
+            <LoginBtns />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
