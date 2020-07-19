28c428d604d73cb9993d402cfb26535c9b4d4db3
sameroso <samerkkr@gmail.com>
2020-07-19 12:04:10 -0300

    changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,29 +1,24 @@
-import axios from "axios"
+import axios from 'axios';
 
+export const fetchUser = () => async (dispatch) => {
+  const response = await axios.get('/api/current');
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 
-export const fetchUser = () => async (dispatch) =>{
-   const response = await axios.get('/api/current')
-    dispatch({type:'FETCH_USER', payload:response.data})
-} 
-
-export const savePassword = (formValues,history) => async (dispatch) => {
-    const response = await axios.post('/api/savepassword', formValues);
-    history.push('/')
-    dispatch({type:"FETCH_USER", payload:response.data})
-
-}
+export const savePassword = (formValues, history) => async (dispatch) => {
+  const response = await axios.post('/api/savepassword', formValues);
+  history.push('/');
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 export const deletePassword = (_id) => async (dispatch) => {
-    const response = await axios.post('/api/deletepassword', _id);
-    dispatch({type:"FETCH_USER", payload:response.data})
-
-}
+  const response = await axios.post('/api/deletepassword', _id);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 export const editPassword = (values) => async (dispatch) => {
-    const response = await axios.patch('/api/updatepassword', values);
-    dispatch({type:"FETCH_USER", payload:response.data})
-
-}
+  const response = await axios.patch('/api/updatepassword', values);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 export const deleteAccount = () => async (dispatch) => {
-    const response = await axios.post('/api/deleteaccount');
-    dispatch({type:"FETCH_USER", payload:response.data})
-
-}
+  const response = await axios.post('/api/deleteaccount');
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
