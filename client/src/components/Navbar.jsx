3f1df46449da67f20279bb61d12e4218e6b5f12d
sameroso <samerkkr@gmail.com>
2020-07-19 13:05:29 -0300

    little bit more adjusts in the style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,88 +7,91 @@ import Modal from './Modal';
 import { deleteAccount } from '../actions';
 
 function Navbar({ auth, deleteAccount }) {
-	const renderProfile = () => {
-		if (auth === false) {
-			return null;
-		} else if (auth === null) {
-			return null;
-		} else {
-			return (
-				<div className="d-flex mr-4">
-					<div className="dropdown my-auto">
-						<button
-							className="dropdown-config dropdown-toggle"
-							type="button"
-							id="dropdownMenu"
-							data-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false">
-							<div className="d-flex">
-								<img
-									className="profile-pic my-auto mr-1"
-									src={auth.profilePic}
-									alt=""
-								/>
-								<p className="text-white mb-0 my-auto">
-									{auth.profileName} &#9660;
-								</p>
-							</div>
-						</button>
-						<div className="dropdown-menu" aria-labelledby="dropdownMenu">
-							<button className="dropdown-item" type="button">
-								About Us
-							</button>
-							<button
-								className="dropdown-item"
-								type="button"
-								data-toggle="modal"
-								data-target="#deleteAccountModal">
-								Delete Account
-							</button>
-							<button className="dropdown-item" type="button">
-								<a href="api/logout" className="logout-link">
-									Logout
-								</a>
-							</button>
-						</div>
-					</div>
-					<Modal
-						id="deleteAccountModal"
-						actionName="delete account"
-						onAction={deleteAccount}
-						btnType="button"
-						type="account"
-					/>
-				</div>
-			);
-		}
-	};
-	return (
-		<div className="background py-2">
-			<div className="container">
-				<div className="row justify-content-between">
-					<Link to="/">
-						<div className="d-flex ml-2">
-							<img
-								className="config-image my-auto"
-								src={passwordLogo}
-								alt="Logo"></img>
-							<h5 className="ml-2 font-header my-auto">
-								<strong>Password</strong>
-								<br />
-								Storage
-							</h5>
-						</div>
-					</Link>
-					{renderProfile()}
-				</div>
-			</div>
-		</div>
-	);
+  const renderProfile = () => {
+    if (auth === false) {
+      return null;
+    } else if (auth === null) {
+      return null;
+    } else {
+      return (
+        <div className="d-flex mr-4">
+          <div className="dropdown my-auto">
+            <button
+              className="dropdown-config dropdown-toggle"
+              type="button"
+              id="dropdownMenu"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <div className="d-flex">
+                <img
+                  className="profile-pic my-auto mr-1"
+                  src={auth.profilePic}
+                  alt=""
+                />
+                <p className="text-white mb-0 my-auto">
+                  {auth.profileName} &#9660;
+                </p>
+              </div>
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenu">
+              <button className="dropdown-item" type="button">
+                About Us
+              </button>
+              <button
+                className="dropdown-item"
+                type="button"
+                data-toggle="modal"
+                data-target="#deleteAccountModal"
+              >
+                Delete Account
+              </button>
+              <button className="dropdown-item" type="button">
+                <a href="api/logout" className="logout-link">
+                  Logout
+                </a>
+              </button>
+            </div>
+          </div>
+          <Modal
+            id="deleteAccountModal"
+            actionName="delete account"
+            onAction={deleteAccount}
+            btnType="button"
+            type="account"
+          />
+        </div>
+      );
+    }
+  };
+  return (
+    <div className="background py-2">
+      <div className="container">
+        <div className="row justify-content-between">
+          <Link to="/" className="link-logo">
+            <div className="d-flex ml-2">
+              <img
+                className="config-image my-auto"
+                src={passwordLogo}
+                alt="Logo"
+              ></img>
+              <h5 className="ml-2 font-header my-auto">
+                <strong>Password</strong>
+                <br />
+                Storage
+              </h5>
+            </div>
+          </Link>
+          {renderProfile()}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-	return { auth: state.auth };
+  return { auth: state.auth };
 };
 
 export default connect(mapStateToProps, { deleteAccount })(Navbar);
